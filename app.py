8e3eb6266f2c62aad3ab63c5d8f422a6030bc5d9
Andrew Galloway <agalloway@coppertreeanalytics.com>
2022-06-02 21:12:45 -0700

    adds alternative message trigger

--- app.py
+++ app.py
@@ -23,24 +23,34 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    # The command issued via the inbound message
+    command = None
+
     if message.author == client.user:
         return
 
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
 
+    # Trigger #1
+    elif message.content.startswith('!panko') and len(message.content) > 6:
+        elements = message.content.split(' ')
+        command = Command(elements[1])
+
+    # Trigger #2
+    elif message.content.startswith('$') and len(message.content) > 1:
+        command_str = message.content[1:].strip()
+        command = Command(command_str)
+
     # Let's go!!!
-    elif message.content.startswith('!panko'):
+    if command is not None:
 
         # Where the message originated from
         if not hasattr(message.channel, 'guild'):
             source = message.channel
         else:
             source = message.channel.guild
-
-        # The message input text
-        elements = message.content.split(' ')
-        command = Command(elements[1])
 
         # Divergent, one-off logic = bad :(
         if command.action == 'about':
@@ -57,6 +67,5 @@ async def on_message(message):
                     await message.add_reaction(response.reaction)
                 if response.message is not None:
                     await message.channel.send(embed=response.message)
-
 
 client.run(config.token)
