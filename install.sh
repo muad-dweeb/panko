a491ef6788f115fc1757f3689c78da5117114c7e
Andrew Galloway <akq.galloway@gmail.com>
2022-09-06 18:23:14 -0700

    installer updates

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 logfile=/var/log/panko.log
 user=$(whoami)
 
 sudo touch $logfile
 sudo chown $user $logfile
+
+python3 -m venv ${HOME}/.virtualenvs/panko
+
+
+symlink=${HOME}/panko
+if [ ! -e $symlink ]; then
+  ln -s $SCRIPT_DIR $symlink
+fi
 
